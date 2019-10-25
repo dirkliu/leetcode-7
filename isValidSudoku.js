71b137797e22c246ddb62a3a0b59127a2fe040fd
dirkliu <liuqi0702@126.com>
2019-10-25 16:14:44 +0800

    位运算！

--- isValidSudoku.js
+++ isValidSudoku.js
@@ -52,40 +52,40 @@ var sudos = [
 // 80ms end
 
 // 60ms
-// var isValidSudoku = function(board) {
-//   const xSetMap = nineZero.concat(),
-//     ySetMap = nineZero.concat(),
-//     areaSetMap = nineZero.concat();
+var isValidSudoku = function(board) {
+  const xSetMap = nineZero.concat(),
+    ySetMap = nineZero.concat(),
+    areaSetMap = nineZero.concat();
 
-//   for (var y = 0; y !== 9; y++) {
-//     for (var x = 0; x !== 9; x++) {
-//       const value = board[y][x];
-//       if (value !== ".") {
-//         const bit = 1 << value;
+  for (var y = 0; y !== 9; y++) {
+    for (var x = 0; x !== 9; x++) {
+      const value = board[y][x];
+      if (value !== ".") {
+        const bit = 1 << value;
 
-//         if ((xSetMap[x] & bit) !== 0) {
-//           return false;
-//         }
-//         if ((ySetMap[y] & bit) !== 0) {
-//           return false;
-//         }
-//         const area = indexMap[y][x];
-//         if ((areaSetMap[area] & bit) !== 0) {
-//           return false;
-//         }
-//         xSetMap[x] |= bit;
-//         ySetMap[y] |= bit;
-//         areaSetMap[area] |= bit;
-//       }
-//     }
-//   }
-//   return true;
-// };
-// const nineNull = new Array(9).fill(null);
-// const nineZero = new Array(9).fill(0);
-// const indexMap = nineNull.map((_, y) =>
-//   nineZero.concat().map((_, x) => Math.floor(y / 3) * 3 + Math.floor(x / 3))
-// );
+        if ((xSetMap[x] & bit) !== 0) {
+          return false;
+        }
+        if ((ySetMap[y] & bit) !== 0) {
+          return false;
+        }
+        const area = indexMap[y][x];
+        if ((areaSetMap[area] & bit) !== 0) {
+          return false;
+        }
+        xSetMap[x] |= bit;
+        ySetMap[y] |= bit;
+        areaSetMap[area] |= bit;
+      }
+    }
+  }
+  return true;
+};
+const nineNull = new Array(9).fill(null);
+const nineZero = new Array(9).fill(0);
+const indexMap = nineNull.map((_, y) =>
+  nineZero.concat().map((_, x) => Math.floor(y / 3) * 3 + Math.floor(x / 3))
+);
 // 60ms end
 
 // 80ms
