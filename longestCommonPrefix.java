6eaf760d8a08f7d14976fa57defbd07c58829a76
dirkliu <liuqi0702@126.com>
2019-11-06 20:31:31 +0800

    最长的公共前缀！

--- longestCommonPrefix.java
+++ longestCommonPrefix.java
@@ -1,4 +1,19 @@
 class Solution {
-    public String longestCommonPrefix(String[] strs) { 
+    public String longestCommonPrefix(String[] strs) {
+      String str = "";
+      int i = 0;
+      int n = strs.length
+      if (n == 0) {
+        return str
+      }
+      for (int i = 0; i < strs[0].length(); i++) {
+        for (int j = 1; j < n; j++) {
+          if i >= strs[j].length() || strs[0][i].toString() != strs[j][i].toString() {
+            return str
+          }          
+        }
+        str += string(strs[0][i])
+      }
+      return str 
     }
 }
